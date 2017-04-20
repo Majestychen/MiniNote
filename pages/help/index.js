96071e6901e9a63d6b15818a62454669e24e80b5
Qin <haoleiqin@qq.com>
2017-04-20 16:39:41 +0800

    update

--- pages/help/index.js
+++ pages/help/index.js
@@ -1,47 +1,41 @@
 //pages/help/index.js
-var Bmob = require('../../lib/bmob.js'); 
+var Bmob = require('../../lib/bmob.js');
+const app = getApp();
 var that;
+function retry() {
+	wx.showModal({
+		title: '网络出问题啦',
+		content: '是否重试',
+		confirmText: '重试',
+		success: function(res) {
+			if(res.confirm) {
+				that.onShow();
+			}
+		}
+	});
+};
 Page({
-  
-  data: {
-  },
 
-  onLoad: function () { 
-      that=this;
-  },
+	data: {},
 
-  onShow:function(){
-          var Diary_note = Bmob.Object.extend("help");
-          var query = new Bmob.Query(Diary_note);   
-          // 排序
-          query.ascending("order");
-          // 查询当前用户数据  
-          query.find({
-            success: function (results) {
-              // 循环处理查询到的数据
-              var temp = [];
-              var temp2 = [];
-              for (var i = 0; i < results.length; i++) {               
-                  temp.push(results[i]);              
-              }
-              that.setData({
-                diaryList: temp
-              })
-            },
-            error: function (error) {
-            	  wx.showModal({
-						    title: '网络出问题啦',
-						    content: '是否重试',
-						    confirmText:'重试', 
-						    success: function(res) {
-						    if (res.confirm) {
-						    	// 如果用户点击了确认按钮
-						    	that.onShow();
-						    }
-						  }
-						});
-            }
-          }); 
-},
- 
+	onLoad: function() {
+		that = this;
+	},
+
+	onShow: function() {
+		var helpDataBase = Bmob.Object.extend("help");
+		var query = new Bmob.Query(helpDataBase);
+		query.ascending("order");
+		query.find({
+			success: function(results) {
+				that.setData({
+					helpData: results
+				})
+			},
+			error: function(error) {
+				retry();
+			}
+		});
+	},
+
 })
