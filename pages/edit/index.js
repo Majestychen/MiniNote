5bde04363df2e04b5f678826057b0f5ed37959f8
Qin <haoleiqin@qq.com>
2017-04-20 15:16:44 +0800

    反馈意见删除截图

--- pages/edit/index.js
+++ pages/edit/index.js
@@ -2,63 +2,20 @@
 const app = getApp();
 const Bmob = require('../../lib/bmob.js');
 const util = app.util;
-var that = {};
-var over = {
-	objectId: 0,
-	input_title: '',
-	input_content: '',
-	editData: [],
-	arrayId: -1,
-};
-var queryParam = [];
+var queryParam = []; //0:openid 1:noteData
 var noteData = [];
-
-function retry() {
-	wx.showModal({
-		title: '网络开小差了',
-		content: '是否重试',
-		confirmText: '重试',
-		success: function(res) {
-			if(res.confirm) {
-				that.onShow();
-			}
-		}
-	})
-};
-
-function changeNoteData(noteDa) {
-	wx.setStorage({
-		key: 'noteData',
-		data: noteDa,
-		success: function(res) {
-			wx.setStorage({
-				key: 'dataChange',
-				data: true,
-				success: function(res) {
-					wx.navigateBack();
-				},
-			})
-		},
-	})
-};
-
-function goSave() {
-	var Diary = Bmob.Object.extend("user_note");
-	var query = new Bmob.Query(Diary);
+var inputData = []; //0:inputTtile//1:inputContent //2:timeOut
+function updateServerData(judge) {
+	var query = new Bmob.Query(Bmob.Object.extend("user_note"));
 	query.get(queryParam[0], {
 		success: function(result) {
-			if(over.input_title != '') {
-				result.set("note_title", over.input_title);
-
-			}
-			if(over.input_content || over.input_content == '') {
-				result.set("note_content", over.input_content);
-			}
+			result.set("note_content", inputData[1]);
+			result.set("note_title", inputData[0]);
 			result.set("note_date", util.getNowTimeformat());
 			result.set("date", new Date().getTime());
 			result.save({
 				success: function(res) {
-					saveNoteData(res, true);
+					saveStorageData(res, judge);
 				}
 			});
 		},
@@ -69,37 +26,25 @@ function goSave() {
 	});
 };
 
-function justSave() {
-	var Diary = Bmob.Object.extend("user_note");
-	var query = new Bmob.Query(Diary);
-	query.get(queryParam[0], {
-		success: function(result) {
-			if(over.input_title != '') {
-				result.set("note_title", over.input_title);
+function changeNoteData(data, saveJudge) {
+	wx.setStorage({
+		key: 'noteData',
+		data: data,
+		success: function(res) {
+			if(saveJudge) {
+				wx.navigateBack();
+			} else {
+				wx.setStorage({
+					key: 'dataChange',
+					data: true,
+				})
 			}
-			if(over.input_content || over.input_content == '') {
-				result.set("note_content", over.input_content);
-			}
-			result.set("note_date", util.getNowTimeformat());
-			result.set("date", new Date().getTime());
-			result.save({
-				success: function(res) {
-					saveNoteData(res, false);
-				}
-			});
 		},
-		error: function(object, error) {
-			console.log(error)
-			wx.showToast({
-				title: '网络故障,请重试',
-				icon: 'loading',
-				duration: 600
-			})
-		}
 	});
+
 };
 
-function saveNoteData(res, save) {
+function saveStorageData(res, saveJudge) {
 	for(var i = 0; i < noteData.length; i++) {
 		if(noteData[i].objectId == res.id) {
 			noteData[i] = {
@@ -112,36 +57,19 @@ function saveNoteData(res, save) {
 
 		}
 	}
-	wx.setStorage({
-		key: 'noteData',
-		data: noteData,
-		success: function() {
-			wx.setStorage({
-				key: 'dataChange',
-				data: true,
-				success: function(res) {
-					if(save) {
-						wx.navigateBack();
-					}
-				},
-			})
-		},
-	});
+	changeNoteData(noteData, saveJudge);
 }
 
 Page({
-	data: {
-		sendBtn: 'sendBtn',
-	},
-
+	data: {},
 	onLoad: function(query) {
-		that = this;
+		var that = this;
 		wx.getSystemInfo({
 			success: function(res) {
 				var tempHeight = res.windowHeight;
 				tempHeight = tempHeight - 85;
 				that.setData({
-					texth: tempHeight,
+					ContentTextHeight: tempHeight,
 				});
 			}
 		})
@@ -153,8 +81,8 @@ Page({
 	},
 
 	onShow: function() {
-		over.input_content = '';
-		over.input_title = '';
+		inputData[1] = '';
+		inputData[0] = '';
 		wx.getStorage({
 			key: 'noteData',
 			success: function(res) {
@@ -165,39 +93,27 @@ Page({
 
 	onReady: function() {},
 
-	zhengzai_input: function(e) {
-		over.input_content = e.detail.value;
-		over.input_title = over.input_content.substr(0, 20);
-		if(over.input_title.indexOf("\n") > -1) {
-			over.input_title = over.input_title.substr(0, over.input_title.indexOf("\n"));
+	contentInput: function(e) {
+		clearTimeout(inputData[2]);
+		inputData[1] = e.detail.value;
+		inputData[0] = inputData[1].substr(0, 20);
+		if(inputData[0].indexOf("\n") > -1) {
+			inputData[0] = inputData[0].substr(0, inputData[0].indexOf("\n"));
 		}
-		justSave();
+		inputData[2] = setTimeout(function() {
+			updateServerData(false);
+		}, 500);
 	},
 
-	// 笔记分享功能
-	/*
-	onShareAppMessage: function() {
-		return {
-			title: over.input_title,
-			path: 'pages/edit/index?id=' + queryParam
-		}
-	},
-	*/
-
-	submit: function(e) {
-		if(over.input_title != '' || over.input_content != '') {
-			goSave();
+	okClick: function(e) {
+		if(inputData[0] != '' || inputData[1] != '') {
+			updateServerData(true);
 		} else {
 			wx.navigateBack()
 		}
 	},
 
-	// 页面卸载
-	onUnload: function() {
+	onUnload: function() {},
 
-	},
-
-	onHide: function() {
-
-	},
+	onHide: function() {},
 });
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,187 +1,159 @@
 //pages/feedback/index.js
-var Bmob = require('../../lib/bmob.js');
-//  正文内容
+const Bmob = require('../../lib/bmob.js');
+const app = getApp();
 var input_zhengzai = '';
-//  标题内容
 var input_title = '';
-//  文章总数
-var list_sum = 0;
-//  呱呱~\(≧▽≦)/~啦啦啦
-var that = {};
-//  测试数据
-var items = ['item1', 'item2', 'item3', 'item4']
-//  用来查询数据的 
-//  这里搞时间   
-var now = new Date();
-//  获取当前年     
-var year = now.getFullYear();
-//  获取当前月
-var month = now.getMonth() + 1;
-//  获取当前日    
-var day = now.getDate();
-// 搞一下全局方法
-var app = getApp();
-// 那个图片的数量和路径
+var chooseImg = [];
 var tempPaths = '';
-var tempPathArr=[];
 var tempFile_num = 0;
+var tempPathArr = [];
+Page({
+	data: {
+		chooseImgBtn: '',
+	},
+	onLoad: function(options) {},
+	onShow: function() {
+		wx.showNavigationBarLoading();
+		setTimeout(function() {
+			wx.hideNavigationBarLoading()
+		}, Math.random() * 2022);
 
-Page(
-    {
-        data: {
-        
-        },
+	},
 
-        //这个应该是载入页面调用的 
-        onLoad: function (options) {
-            that = this;
-   
-        },
-        
-    // 选择图片事件
-	chooseimg: function() {
+	onReady: function() {},
+	chooseImgClick: function() {
+		var that = this;
+		wx.showNavigationBarLoading();
 		wx.chooseImage({
 			count: 1, // 默认9
 			sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
 			sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
 			success: function(res) {
 				// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
-			 		var Diary_q_1 = Bmob.Object.extend("user_feedback");
-					var diary_q_1 = new Diary_q_1();
-					var name = res.tempFilePaths+".jpg"; //上传的图片的别名
-					var file = new Bmob.File(name, res.tempFilePaths);
-                    wx.showToast({
-                        title: '正在上传',
-                        icon: 'loading',
-                        duration: 28000
-                    })
-					// 最多上传6张图
-					if (tempPathArr.length<7) {
+				var fbBmob = Bmob.Object.extend("user_feedback");
+				var addFeedback = new fbBmob();
+				var name = res.tempFilePaths + ".jpg"; //上传的图片的别名
+				var file = new Bmob.File(name, res.tempFilePaths);
+				// 最多上传10张图
+				if(tempPathArr.length < 11) {
 					file.save().then(function(res) {
-						 tempPathArr.push(res.url());				 		
-							that.setData({
-								src: tempPathArr,
-							}); 
-                            wx.hideToast();
-					}, function(error) {
+						tempPathArr.push(res.url());
+						that.setData({
+							src: tempPathArr,
+						});
+						wx.hideNavigationBarLoading();
+					}, function(error) {});
+				} else {
+					that.setData({
+						chooseImgBtn: 'hide',
 					});
-					} else{
-						wx.showToast({
-						  title: '最多上传六张图片',
-						  icon: 'loading',
-						  duration: 2000
-						})
-					}
-
+				}
 
 			}
-		}) 
+		})
 	},
- 
-        // onShow开始
-        onShow: function () {
-            wx.showNavigationBarLoading();
-            setTimeout(function () { wx.hideNavigationBarLoading() }, Math.random() * 2022);
-            this.setData({
-                actionSheetHidden: !this.data.actionSheetHidden
-            })
-            //底部导航显示啥的
-            that.setData({
-                bottom_bar_class: "bottom_bar"
-            });
-        },
-        // onShow结束
 
+	deleteImgClick: function(e) {
+		var that = this;
+		var id = e.currentTarget.id ? e.currentTarget.id : e.target.id;
+		if(tempPathArr.length == 1) {
+			tempPathArr = [];
+			that.setData({
+				src: tempPathArr,
+			});
+		} else {
+			for(var i = 0; i < tempPathArr.length; i++) {
+				if(id == tempPathArr[i]) {
+					tempPathArr.splice(i, 1);
+					that.setData({
+						src: tempPathArr,
+					});
+				}
+			}
+		}
 
-        onReady: function () {
+	},
 
-        },
+	// 反馈内容
+	q_title_input: function(e) {
+		input_title = e.detail.value;
+	},
 
-        // 反馈内容
-        q_title_input: function (e) {
-            input_title = e.detail.value;
-        },
+	// 联系方式
+	zhengzai_input: function(e) {
+		input_zhengzai = e.detail.value;
+	},
 
-        // 联系方式
-        zhengzai_input: function (e) {
-            input_zhengzai = e.detail.value;
-        },
+	// 提交笔记事件
+	submit: function(e) {
+		var Diary_q_1 = Bmob.Object.extend("user_feedback");
+		var diary_q_1 = new Diary_q_1();
+		var q_temp_username = ''
+		wx.getStorage({
+			key: 'user_openid',
+			success: function(res) {
+				diary_q_1.set("user_openid_wechat", res.data);
+				if(input_zhengzai) {
+					if(tempPaths.length > 0) {
+						var name = res.data + '/' + tempFile_num + ".jpg"; //上传的图片的别名
+						var file = new Bmob.File(name, tempPaths);
+						file.save().then(function(res) {}, function(error) {});
+						// 状态
+						diary_q_1.set("status", '有图' + name);
+					}
 
-        // 提交笔记事件
-        submit: function (e) {
-            var Diary_q_1 = Bmob.Object.extend("user_feedback");
-            var diary_q_1 = new Diary_q_1();
-            var q_temp_username = ''
-            wx.getStorage({
-                key: 'user_openid',
-                success: function (res) {
-                    diary_q_1.set("user_openid_wechat", res.data);
-                    if (input_zhengzai) {
-                    if (tempPaths.length > 0) {
-                        var name = res.data + '/' + tempFile_num + ".jpg";//上传的图片的别名
-                        var file = new Bmob.File(name, tempPaths );
-                        file.save().then(function (res) {
-                        }, function (error) {
-                        });
-                         // 状态
-                        diary_q_1.set("status", '有图'+name);
-                    }       
+					// 用户的联系方式
+					diary_q_1.set("contact", input_title);
 
-                        // 用户的联系方式
-                        diary_q_1.set("contact", input_title);
-             
-                        diary_q_1.set("feedback", input_zhengzai);
-                        //添加数据，第一个入口参数是null
-                        diary_q_1.save(null, {
-                            success: function (result) {
-                                // 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
-                                // console.log("日记创建成功, objectId:"+result.id);
+					diary_q_1.set("feedback", input_zhengzai);
+					//添加数据，第一个入口参数是null
+					diary_q_1.save(null, {
+						success: function(result) {
+							// 添加成功，返回成功之后的objectId（注意：返回的属性名字是id，不是objectId），你还可以在Bmob的Web管理后台看到对应的数据
+							// console.log("日记创建成功, objectId:"+result.id);
 
-                                wx.showToast({
-                                    title: '感谢您的意见 ^_^',
-                                    icon: 'success',
-                                    duration: 1226
-                                })
-                                //  返回主界面
-                                // 在C页面内 navigateBack，将返回A页面
-                                setTimeout(function () {
-                                    wx.navigateBack({
-                                        url: '../mine/index'
-                                    })
-                                }, 1226);
+							wx.showToast({
+								title: '感谢您的意见 ^_^',
+								icon: 'success',
+								duration: 1226
+							})
+							//  返回主界面
+							// 在C页面内 navigateBack，将返回A页面
+							setTimeout(function() {
+								wx.navigateBack({
+									url: '../mine/index'
+								})
+							}, 1226);
 
-                            },
-                            error: function (result, error) {
-                                wx.showToast({
-                                    title: '网络故障 请重试',
-                                    icon: 'loading',
-                                    duration: 1226
-                                })
-                            }
-                        });
+						},
+						error: function(result, error) {
+							wx.showToast({
+								title: '网络故障 请重试',
+								icon: 'loading',
+								duration: 1226
+							})
+						}
+					});
 
+				} else {
+					wx.showToast({
+						title: '空空的~',
+						icon: 'loading',
+						duration: 1222
+					})
+				}
 
-                    }
-                    else {
-                        wx.showToast({
-                            title: '空空的~',
-                            icon: 'loading',
-                            duration: 1222
-                        })
-                    }
+			}
+		})
 
-                }
-            })
+		//  onsubmit结束
+	},
 
-
-            //  onsubmit结束
-        },
-
-        // 取消事件
-        cancle: function () {
-            wx.navigateBack({
-                url: '../mine/index'
-            })
-        }
-        // 取消事件结束
-    });
+	// 取消事件
+	cancle: function() {
+		wx.navigateBack({
+			url: '../mine/index'
+		})
+	}
+	// 取消事件结束
+});
